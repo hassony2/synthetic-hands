0fb10b000e9cc9f98bb762da9db0bae341d11d8b
Yana Hasson <yana.hasson@inria.fr>
2017-06-15 19:44:50 +0200

    Refactor depth reading

--- blender-scripts/read_files.py
+++ blender-scripts/read_files.py
@@ -3,16 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-depth = cv2.imread('/home/local2/yhasson/first-person-action-recognition/data/blender-renders/Annots/Image0081.exr', flags=3)
-print(depth)
-print(type(depth))
-print(np.max(depth))
-print(np.min(depth))
-print(np.unique(depth))
-print(depth.shape)
+from utils import filesys
+
+im_path = '/home/local2/yhasson/first-person-action-recognition/data/blender-renders/Annots/Image0081.exr'
+# Read depth
+depth = filesys.read_depth(im_path)
 im_range = 1
 readable_depth = np.clip(depth, 0.8, im_range)
-plt.imshow(readable_depth[:, :, 2]/im_range)
+plt.imshow(readable_depth/im_range)
 plt.show()
 
 
