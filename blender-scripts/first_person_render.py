0d9e5d1dab5bec9feb896792023ad320bdc3f21f
Yana Hasson <yana.hasson@inria.fr>
2017-06-16 11:43:18 +0200

    Add object segmentation and place cycle nodes

--- blender-scripts/first_person_render.py
+++ blender-scripts/first_person_render.py
@@ -59,35 +59,48 @@ for n in node_tree.nodes:
 # Create nodes
 inp_node = node_tree.nodes.new(type="CompositorNodeImage")
 inp_node.image = background_img
+inp_node.location = -400, 200
 
 scale_node = node_tree.nodes.new(type="CompositorNodeScale")
 scale_node.space = "RENDER_SIZE"
 scale_node.frame_method = "CROP"
+scale_node.location = -200, 200
 node_tree.links.new(inp_node.outputs[0], scale_node.inputs[0])
 
 render_node = node_tree.nodes.new(type="CompositorNodeRLayers")
+render_node.location = -400, -200
 
 alpha_node = node_tree.nodes.new(type="CompositorNodeAlphaOver")
+alpha_node.location = 0, 200
 node_tree.links.new(scale_node.outputs[0], alpha_node.inputs[1])
 node_tree.links.new(render_node.outputs[0], alpha_node.inputs[2])
 
 comp_node = node_tree.nodes.new(type="CompositorNodeComposite")
+comp_node.location = 200, 200
 node_tree.links.new(alpha_node.outputs[0], comp_node.inputs[0])
 
 # Add depth
 depth_node = node_tree.nodes.new('CompositorNodeOutputFile')
 depth_node.format.file_format = 'OPEN_EXR'
 depth_node.base_path = depth_folder
+depth_node.location = 200, 0
 node_tree.links.new(render_node.outputs['Z'], depth_node.inputs[0])
 
 # Add segmentation
+# Activate 'IndexMA' output for render layer
 scene.render.layers['RenderLayer'].use_pass_material_index = True
 
+# Set material index
 body_mat = bpy.data.materials['Material.003']
 body_mat.pass_index = 1
+mug_mat = bpy.data.materials['mia_material_x2SG']
+mug_mat.pass_index = 2
+
+# Add material index renderint (<==> segmentation)
 segm_node = node_tree.nodes.new('CompositorNodeOutputFile')
 segm_node.format.file_format = 'OPEN_EXR'
 segm_node.base_path = segm_folder
+segm_node.location = 200, -200
 node_tree.links.new(render_node.outputs['IndexMA'], segm_node.inputs[0])
 
 if render:
