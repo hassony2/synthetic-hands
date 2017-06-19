c91e4d0d56285be51ae7082bf3b00d03eccd4e4b
Yana Hasson <yana.hasson@inria.fr>
2017-06-19 16:54:16 +0200

    Add timeit decorator

---
+++ blender-scripts/utils/debug.py
@@ -0,0 +1,11 @@
+import time
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        print('[TimeIt] func: "{}" run in {}s'.format(method.__name__, te - ts))
+        return result
+    return timed
