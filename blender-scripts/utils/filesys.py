7d0fcf2f1bba1678e1b1947f4fabc9cd435b1151
Yana Hasson <yana.hasson@inria.fr>
2017-09-26 14:28:07 +0200

    Fix mkdir function

--- blender-scripts/utils/filesys.py
+++ blender-scripts/utils/filesys.py
@@ -5,8 +5,10 @@ def create_dir(path):
     """
     Creates dir if absent
     """
-    directory = os.path.dirname(path)
+    # Extract folder name if filepath is given
+    if '.' in os.path.basename(path):
+        directory = os.path.dirname(path)
+    else:
+        directory = path
     if not os.path.exists(directory):
         os.makedirs(directory)
-
-
